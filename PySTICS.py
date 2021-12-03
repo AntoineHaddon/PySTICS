1ab7c2f439acfd094423d3fb2e6e655add88f34e
antoine.haddon <antoine.haddon@inrae.fr>
2021-12-03 12:54:35 +0100

    add example

--- PySTICS.py
+++ PySTICS.py
@@ -57,18 +57,3 @@ class usm():
 
 
 
-
-if __name__ == "__main__":
-	dir_corn2013 = '/home/ahaddon/Dropbox/Work/ReUse/code/stics/corn/'
-	JavaSticsDir = '/home/ahaddon/Programs/JavaSTICS-1.41-stics-9.1/'
-
-	corn2013 = usm(dir_corn2013,'maize_ref_2013',JavaSticsDir) 
-	# corn2013.run()
-
-	simData = corn2013.loadData()
-	# print(simData)
-	# import matplotlib.pyplot as plt
-	# [plt.plot(simData['jul'], simData['HR('+str(i)+')'] ) for i in range(1,5)]
-	# plt.show()
-
-
